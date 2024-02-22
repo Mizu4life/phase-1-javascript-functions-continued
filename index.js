7ff8884cd27934438be3928bd0074c657c8e077e
Zainab Al-Najjar <eng.zainab.alnajar@gmail.com>
2024-02-23 02:25:10 +0300

    Completed assignment!

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function saturdayFun(argument="roller-skate"){
+    return `This Saturday, I want to ${argument}!`;
+}
+function mondayWork(argument="go to the office"){
+    return`This Monday, I will ${argument}.`;
+}
+function wrapAdjective(symbol){
+    const inner = function(parameter="special"){
+        return `You are ${symbol}${parameter}${symbol}!`;
+    }
+    return inner;
+}
